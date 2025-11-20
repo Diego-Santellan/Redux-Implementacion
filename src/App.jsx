183a775de79ba61986c5feacfa992e4f7504c4df
diego santellan <diego.santellan2001@gmail.com>
2025-11-20 19:17:30 -0300

    creacion de la UI para el CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import ProductsList from './components/ProductsList';
 import UsersList from './components/UsersList';
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
 
       <h1>Vite + React + Redux Toolkit</h1>
       <UsersList /> {/* traemos la lista de los usuarios ---> tener cuidado con la importacion aveces no se hace automatico */}
+      <ProductsList />
+      
     </>
   )
 }
