8959eac1c3d5d2d6a90fb146bddd7cad1bf8e419
diego santellan <diego.santellan2001@gmail.com>
2025-11-20 20:10:28 -0300

    operacion de lectura del CRUD

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,11 +1,31 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { readProducts } from "../redux/productsSlice";
+
 const ProductsList = () =>{
+
+    const products = useSelector(state => state.products);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/products") //la ruta a nuestra DB la cual creamos con JSON-Server
+            .then((res) => {
+                console.log(res);
+                dispatch(readProducts(res.data));
+            }).catch((err) => {console.error(err)});//captura de errores
+
+    },[dispatch])
+
     return(
         <>
             <h2>CRUD de Productos</h2>
             <h3>Lista de Productos</h3>
             <ul>
-                <li>Productos... </li>
-
+                {products.data.map( (product) => (
+                    <li key={product.id}>{product.name}</li>
+                ))}
             </ul>
             <aside>
                 <input type="text" />
--- src/redux/productsSlice.js
+++ src/redux/productsSlice.js
@@ -2,14 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const productsSlice = createSlice ({
     name: "products",
-    initialState:[],
+    initialState:{ //enves de que sea un array, decidimos que seria mejor un objeto para poder detallar mas informacion de serlo necesario
+        data:[]
+    },
     reducers:{/* funciones/logicas ---> hacemosun CRUD (Create, Read, Update, Delete) */
         createProduct :(state, action)=>{},
-        readProduct :(state, action)=>{},
+        readProducts :(state, action)=>{
+            state.data = action.payload;
+        },
         updateProduct :(state, action)=>{},
         deleteProduct :(state, action)=>{}
     },
 });
 
-export const { createProduct, readProduct, updateProduct, deleteProduct}  = productsSlice.actions; //exportamos las funcionalidades
+export const { createProduct, readProducts, updateProduct, deleteProduct}  = productsSlice.actions; //exportamos las funcionalidades
 export default productsSlice.reducer; //exportamos el reducer para que reduxtoolkit haga la convinacion
